d856703a6e5c9c6d1fcf4a3323d9f8e28789a1eb
wo3kie <wo3kie@hotmail.com>
2016-03-12 10:59:07 +0000

    Lua

--- lua/main.lua
+++ lua/main.lua
@@ -1,4 +1,31 @@
-function f(x, y)
-    return x * y
+function hello()
+    print "Hello World"
+end
+
+function get1()
+    return 1
+end
+
+function getPi()
+    return 3.14
+end
+
+function factorial(x)
+    if (x == 0 ) then
+        return 1
+    else
+        return x * factorial(x-1)
+    end
+
+end
+
+function pow(x, y)
+    local result = x
+
+    for i=1, (y-1) do
+        result = result * x
+    end
+
+    return result
 end
 
