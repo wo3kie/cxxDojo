5895c48dea950ef8b71f72d81e820d2101fc7cb6
wo3kie <wo3kie@hotmail.com>
2016-03-20 12:18:56 +0000

    mandelbrot

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -14,22 +14,22 @@
 
 #include <iostream>
 
-char digit(int r, int t){
-    return '0' + (r % 10);
+char digit(int iteration, int maxIteration){
+    return '0' + (iteration % 10);
 }
 
-char hash(int r, int t){
-    return (r >= t) ? '#' : ' ';
+char hash(int iteration, int maxIteration){
+    return (iteration >= maxIteration) ? '#' : ' ';
 }
 
-char asciiArt1(int r, int t){
+char asciiArt1(int iteration, int maxIteration){
     char const * const ascii = " .,:;^*&#@";
-    return ascii[ r % 10 ];
+    return ascii[ iteration % 10 ];
 }
 
-char asciiArt2(int r, int t){
+char asciiArt2(int iteration, int maxIteration){
     char const * const ascii = "@#&*^;:,. ";
-    return ascii[ r % 10 ];
+    return ascii[ iteration % 10 ];
 }
 
 void mandelbrot( char (*getAscii)(int, int) ){
@@ -41,24 +41,24 @@ void mandelbrot( char (*getAscii)(int, int) ){
     int height = scale * 60;
     double const hStep = 4.0 / height;
 
-    int const threshold = 50;
+    int const maxIteration = 50;
 
     for( int h = 0 ; h < height ; ++h ){
         for( int w = 0 ; w < width ; ++w ){
-            double const n = wStep * w - 2;
-            double const u = hStep * h - 2;
+            double const x0 = wStep * w - 2;
+            double const y0 = hStep * h - 2;
             
-            int r = 0;
+            int iteration = 0;
             double x = 0;
             double y = 0;
 
-            for( r = 0 ; x * x + y * y < 4 && r < threshold ; ++r ){
-                double const temp = x * x - y * y + n;
-                y = 2 * x * y + u;
+            for( ; x * x + y * y < 4 && iteration < maxIteration ; ++iteration ){
+                double const temp = x * x - y * y + x0;
+                y = 2 * x * y + y0;
                 x = temp;
             }
 
-            std::cout << getAscii(r, threshold);
+            std::cout << getAscii(iteration, maxIteration);
         }
 
         std::cout << std::endl;
