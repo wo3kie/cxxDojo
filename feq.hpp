3792b16900476ee726ecfcbf55420cd1bc02d603
wo3kie <wo3kie@hotmail.com>
2016-03-22 18:37:42 +0000

    feq

--- feq.hpp
+++ feq.hpp
@@ -9,17 +9,19 @@
 #ifndef CXX_DOJO_FEQ_HPP
 #define CXX_DOJO_FEQ_HPP
 
+inline
 constexpr bool feq( float const f1, float const f2, float const eps = 0.0001f ){
-    return (f1-f2) < eps || (f2 - f1) < eps;
+    return (f1 - f2) < eps || (f2 - f1) < eps;
 }
 
+inline
 constexpr bool feq( double const f1, double const f2, double const eps = 0.0001 ){
-    return (f1-f2) < eps || (f2 - f1) < eps;
+    return (f1 - f2) < eps || (f2 - f1) < eps;
 }
 
-
+inline
 constexpr bool feq( long double const f1, long double const f2, long double const eps = 0.0001 ){
-    return (f1-f2) < eps || (f2 - f1) < eps;
+    return (f1 - f2) < eps || (f2 - f1) < eps;
 }
 
 #endif
