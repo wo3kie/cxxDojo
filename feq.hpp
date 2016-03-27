5f2741cab617904b01ca9a5d8dacbf52d5a96e13
wo3kie <wo3kie@hotmail.com>
2016-03-27 10:35:41 +0100

    feq

--- feq.hpp
+++ feq.hpp
@@ -11,18 +11,17 @@
 
 inline
 constexpr bool feq( float const f1, float const f2, float const eps = 0.0001f ){
-    return (f1 - f2) < eps || (f2 - f1) < eps;
+    return ((f1 - f2) >= 0 ? (f1 - f2) : (f2 - f1)) < eps;
 }
 
 inline
 constexpr bool feq( double const f1, double const f2, double const eps = 0.0001 ){
-    return (f1 - f2) < eps || (f2 - f1) < eps;
+    return ((f1 - f2) >= 0 ? (f1 - f2) : (f2 - f1)) < eps;
 }
 
 inline
 constexpr bool feq( long double const f1, long double const f2, long double const eps = 0.0001 ){
-    return (f1 - f2) < eps || (f2 - f1) < eps;
+    return ((f1 - f2) >= 0 ? (f1 - f2) : (f2 - f1)) < eps;
 }
 
 #endif
-
