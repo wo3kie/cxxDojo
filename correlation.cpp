0aa24a3254993a8a9569b1527449f0b6107a0d56
wo3kie <wo3kie@hotmail.com>
2016-03-28 11:17:04 +0100

    correlation

--- correlation.cpp
+++ correlation.cpp
@@ -6,7 +6,7 @@
  *      Lukasz Czerwinski
  *
  * Compilation:
- *      g++ --std=c++11 correlation.cpp -o correlation
+ *      g++ --std=c++11 correlation.cpp -o correlation -lgsl -lgslcblas -lm
  *
  * Usage:
  *      $ ./correlation
@@ -14,14 +14,35 @@
 
 #include <cassert>
 
+#include <gsl/gsl_statistics.h>
+
 #include "./feq.hpp"
 #include "./correlation.hpp"
 
 int main(){
-    double const correlationValue = correlation<double>(
-        { 1.2, 2.0, 2.5, 4.0, 3.0, 6.0, 5.5, 6.3, 7.1, 5.4 },
-        { 3.4, 3.3, 3.0, 5.5, 1.2, 2.4, 3.2, 3.1, 2.9, 3.2 }
-    );
+    std::vector<double> const array1{
+        1.2, 2.0, 2.5, 4.0, 3.0, 6.0, 5.5, 6.3, 7.1, 5.4
+    };
 
-    assert(feq(correlationValue, -0.050601));
+    std::vector<double> const array2{
+        3.4, 3.3, 3.0, 5.5, 1.2, 2.4, 3.2, 3.1, 2.9, 3.2
+    };
+
+    {
+        double const correlationValue = correlation<double>(
+            array1,
+            array2
+        );
+
+        assert(feq(correlationValue, -0.050601));
+    }
+
+    {
+        double const correlationValue = gsl_stats_correlation(
+            &array1.front(), 1,
+            &array2.front(), 1, array2.size()
+        );
+
+        assert(feq(correlationValue, -0.050601));
+    }
 }
