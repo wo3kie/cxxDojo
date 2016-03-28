ca37cf83e8f08a7d078e6bdf62726ad5f0305219
wo3kie <wo3kie@hotmail.com>
2016-03-28 11:16:52 +0100

    mean

--- mean.cpp
+++ mean.cpp
@@ -6,7 +6,7 @@
  *      Lukasz Czerwinski
  *
  * Compilation:
- *      g++ --std=c++11 mean.cpp -o mean
+ *      g++ --std=c++11 mean.cpp -o mean -lgsl -lgslcblas -lm
  *
  * Usage:
  *      $ ./mean
@@ -14,9 +14,18 @@
 
 #include <cassert>
 
+#include <gsl/gsl_statistics.h>
+
+#include "./feq.hpp"
 #include "./mean.hpp"
 
 int main(){
-    assert( 2.0 == mean<double>( { 1.0, 2.0, 3.0 } ) );
-    assert( 0.0 == mean<double>( { - 1.0, 0, 1.0 } ) );
+    std::vector<double> const array1{ 1.0, 2.0, 3.0 };
+    std::vector<double> const array2{ - 1.0, 0, 1.0 };
+
+    assert( feq( 2.0, mean<double>( array1 ) ) );
+    assert( feq( 0.0, mean<double>( array2 ) ) );
+
+    assert( feq( 2.0, gsl_stats_mean( &array1.front(), 1, array1.size() ) ) );
+    assert( feq( 0.0, gsl_stats_mean( &array2.front(), 1, array2.size() ) ) );
 }
