35925a4f7acb75f5fa22717e1facd51d8a5c6c52
wo3kie <wo3kie@hotmail.com>
2016-03-28 11:17:36 +0100

    variance

--- variance.cpp
+++ variance.cpp
@@ -6,7 +6,7 @@
  *      Lukasz Czerwinski
  *
  * Compilation:
- *      g++ --std=c++11 variance.cpp -o variance
+ *      g++ --std=c++11 variance.cpp -o variance -lgsl -lgslcblas -lm
  *
  * Usage:
  *      $ ./variance
@@ -14,9 +14,14 @@
 
 #include <cassert>
 
+#include <gsl/gsl_statistics.h>
+
 #include "./feq.hpp"
 #include "./variance.hpp"
 
 int main(){
-    assert( feq(3.067, variance<double>( { 7, 6, 8, 4, 2, 7, 6, 7, 6, 5 } ) ) );
+    std::vector<double> array{ 7, 6, 8, 4, 2, 7, 6, 7, 6, 5 };
+
+    assert( feq( 3.0666, variance<double>( array ) ) );
+    assert( feq( 3.0666, gsl_stats_variance( &array.front(), 1, array.size() ) ) );
 }
