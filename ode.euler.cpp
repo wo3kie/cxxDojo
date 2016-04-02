c402867ac3672c9add9229768305f63b283de95a
wo3kie <wo3kie@hotmail.com>
2016-04-02 11:38:13 +0100

    ode.euler.cpp

--- ode.euler.cpp
+++ ode.euler.cpp
@@ -17,33 +17,51 @@
 
 #include "./feq.hpp"
 
-double solveOdeEuler(
+double solveOdeImpl(
+    std::function<double (double, double, double)> approx,
+    double x0,
+    double const xN,
+    double const h,
+    double y0
+){
+    while(x0 + h <= xN){
+        y0 += approx(x0, x0 + h, y0);
+        x0 += h;
+    }
+
+    return y0;
+}
+
+double solveOdeEulerForward(
     std::function<double (double, double)> f,
     double x0,
     double xN,
     double h,
     double y0
 ){
-    double x = x0 + h;
-    double y = y0;
+    auto approx = [&f](double x0, double x1, double y){
+        double const h = x1 - x0;
+        return h * f(x0, y);
+    };
 
-    while(x <= xN){
-        {
-            // basic
-            // y = y + h * f(x - h, y);
-        }
+    return solveOdeImpl(approx, x0, xN, h, y0);
+}
 
-        {
-            // midpoint
-            double const k1 = h * f(x - h, y);
-            double const k2 = h * f(x - h/2, y + h*k1/2);
-            y = y + k2;
-        }
+double solveOdeEulerMidpoint(
+    std::function<double (double, double)> f,
+    double x0,
+    double xN,
+    double h,
+    double y0
+){
+    auto approx = [&f](double x0, double x1, double y){
+        double const h = (x1 - x0);
+        double const k1 = h * f(x0, y);
+        double const k2 = h * f(x0 + h / 2, y + h * k1 / 2);
+        return k2;
+    };
 
-        x += h;
-    }
-
-    return y;
+    return solveOdeImpl(approx, x0, xN, h, y0);
 }
 
 double f(double x, double y){
@@ -52,6 +70,9 @@ double f(double x, double y){
     return x * x;
 }
 
+#include <iostream>
+
 int main(){
-    assert(feq(9.9775, solveOdeEuler(f, 0, 3, 0.3, 1)));
+    assert(feq(8.6950, solveOdeEulerForward(f, 0, 3, 0.3, 1)));
+    assert(feq(9.9775, solveOdeEulerMidpoint(f, 0, 3, 0.3, 1)));
 }
