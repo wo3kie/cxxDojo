2f08cc4c6da224530a7aa843a14da87ea486a388
wo3kie <wo3kie@hotmail.com>
2016-04-08 18:52:04 +0100

    matrix

--- matrix.cpp
+++ matrix.cpp
@@ -9,198 +9,39 @@
  *      g++ --std=c++11 matrix.cpp -o matrix
  *
  * Usage:
- *      $ ./matrix 
- *      nmp: 743355µs
- *      npm: 1925037µs
- *      mnp: 721955µs
- *      mpn: 793389µs
- *      pnm: 1738803µs
- *      pmn: 962802µs
+ *      $ ./matrix
  */
 
-/*
- * multiply( [N][M] * [P][N] ) : [P][M]
- * 
- *             <---- P ---->
- *             ^ .
- *             |   .
- *             N     .
- *             |        .
- *             V           .
- *
- * <-- N -->   <---- P ---->
- * ^.          ^ .
- * |  .        |   .
- * M   .       M     .
- * |     .     |        .
- * V       .   V           .
- *
- *
- *         Avg.   | 1     | 2     | 3     | 4     | 5     | 6     | 7     |
- *
- * N M P    472116  468465  466858  466395  465372  483760  478316  475649
- * M N P    450503  446093  447004  448114  450932  457181  454447  449752
- *
- * M P N    680446  492579  969800  923971  605525  527605  503998  739647
- * P M N    793965  632770  1033893 985312  656934  640927  715452  892465
- *
- * P N M   1349704 1391133 1103441 1152507 1610877 1365716 1402298 1421957
- * N P M   1342003 1359540 1098046 1159678 1589834 1410942 1357497 1418485
- *
- */
+#include <cassert>
 
-#include <cstdlib>
+#include "./matrix.hpp"
 
-#include <chrono>
-#include <iostream>
-
-template< int N, int M, typename F >
-void fill(
-    int (& matrix)[ N ][ M ],
-    F f
-)
-{
-    for( int n = 0 ; n < N ; ++n )
-        for( int m = 0 ; m < M ; ++m )
-            matrix[ n ][ m ] = f();
-}
-
-template< int N, int M, int P >
-void multiply1(
-    int (& matrix1)[ N ][ M ],
-    int (& matrix2)[ P ][ N ],
-    int (& result )[ P ][ M ]
-)
-{
-    for( int n = 0 ; n < N ; ++ n )
-        for( int m = 0 ; m < M ; ++ m )
-            for( int p = 0 ; p < P ; ++ p )
-                result[ m ][ p ] += matrix1[ n ][ m ] * matrix2[ m ][ n ];
-}
-
-template< int N, int M, int P >
-void multiply2(
-    int (& matrix1)[ N ][ M ],
-    int (& matrix2)[ P ][ N ],
-    int (& result )[ P ][ M ]
-)
-{
-    for( int n = 0 ; n < N ; ++ n )
-            for( int p = 0 ; p < P ; ++ p )
-        for( int m = 0 ; m < M ; ++ m )
-                result[ m ][ p ] += matrix1[ n ][ m ] * matrix2[ m ][ n ];
-}
-
-template< int N, int M, int P >
-void multiply3(
-    int (& matrix1)[ N ][ M ],
-    int (& matrix2)[ P ][ N ],
-    int (& result )[ P ][ M ]
-)
-{
-        for( int m = 0 ; m < M ; ++ m )
-    for( int n = 0 ; n < N ; ++ n )
-            for( int p = 0 ; p < P ; ++ p )
-                result[ m ][ p ] += matrix1[ n ][ m ] * matrix2[ m ][ n ];
-}
-
-template< int N, int M, int P >
-void multiply4(
-    int (& matrix1)[ N ][ M ],
-    int (& matrix2)[ P ][ N ],
-    int (& result )[ P ][ M ]
-)
-{
-        for( int m = 0 ; m < M ; ++ m )
-            for( int p = 0 ; p < P ; ++ p )
-    for( int n = 0 ; n < N ; ++ n )
-                result[ m ][ p ] += matrix1[ n ][ m ] * matrix2[ m ][ n ];
-}
-
-template< int N, int M, int P >
-void multiply5(
-    int (& matrix1)[ N ][ M ],
-    int (& matrix2)[ P ][ N ],
-    int (& result )[ P ][ M ]
-)
-{
-            for( int p = 0 ; p < P ; ++ p )
-    for( int n = 0 ; n < N ; ++ n )
-        for( int m = 0 ; m < M ; ++ m )
-                result[ m ][ p ] += matrix1[ n ][ m ] * matrix2[ m ][ n ];
-}
-
-template< int N, int M, int P >
-void multiply6(
-    int (& matrix1)[ N ][ M ],
-    int (& matrix2)[ P ][ N ],
-    int (& result )[ P ][ M ]
-)
-{
-            for( int p = 0 ; p < P ; ++ p )
-        for( int m = 0 ; m < M ; ++ m )
-    for( int n = 0 ; n < N ; ++ n )
-                result[ m ][ p ] += matrix1[ n ][ m ] * matrix2[ m ][ n ];
-}
-
-int main()
-{
-    int matrix1[ 400 ][ 600 ];
-    int matrix2[ 600 ][ 400 ];
-    int matrix3[ 600 ][ 600 ];
-
-    fill( matrix1, rand );
-    fill( matrix2, rand );
-    fill( matrix3, [](){ return 0; } );
-
-    using namespace std::chrono;
-    
+int main(){
     {
-        const auto start = high_resolution_clock::now();
-        multiply1( matrix1, matrix2, matrix3 );
-        const auto end = high_resolution_clock::now();
-
-        std::cout << "nmp: " << duration_cast< microseconds >(end - start).count() << "µs" << std::endl;
+        Matrix const m1(2, 3, 1);
+        Matrix const m2{{1, 1, 1}, {1, 1, 1}};
+        assert(m1 == m2);
     }
-
     {
-        const auto start = high_resolution_clock::now();
-        multiply2( matrix1, matrix2, matrix3 );
-        const auto end = high_resolution_clock::now();
-
-        std::cout << "npm: " << duration_cast< microseconds >(end - start).count() << "µs" << std::endl;
+        Matrix const m1(2, 3, 1);
+        Matrix const m2{{1, 1}, {1, 1}, {1, 1}};
+        assert(m1 != m2);
     }
-
     {
-        const auto start = high_resolution_clock::now();
-        multiply3( matrix1, matrix2, matrix3 );
-        const auto end = high_resolution_clock::now();
-
-        std::cout << "mnp: " << duration_cast< microseconds >(end - start).count() << "µs" << std::endl;
+        Matrix const m1{{1, 2}, {3, 4}, {5, 6}};
+        Matrix const expected{{2, 4}, {6, 8}, {10, 12}};
+        assert(m1 * 2 == expected);
     }
-
     {
-        const auto start = high_resolution_clock::now();
-        multiply4( matrix1, matrix2, matrix3 );
-        const auto end = high_resolution_clock::now();
-
-        std::cout << "mpn: " << duration_cast< microseconds >(end - start).count() << "µs" << std::endl;
+        Matrix const m1{{1, 2}, {3, 4}, {5, 6}};
+        Matrix const m2{{7, 8}, {9, 0}, {1, 2}};
+        Matrix const expected{{8, 10}, {12, 4}, {6, 8}};
+        assert(m1 + m2 == expected);
     }
-
     {
-        const auto start = high_resolution_clock::now();
-        multiply5( matrix1, matrix2, matrix3 );
-        const auto end = high_resolution_clock::now();
-
-        std::cout << "pnm: " << duration_cast< microseconds >(end - start).count() << "µs" << std::endl;
-    }
-
-    {
-        const auto start = high_resolution_clock::now();
-        multiply6( matrix1, matrix2, matrix3 );
-        const auto end = high_resolution_clock::now();
-
-        std::cout << "pmn: " << duration_cast< microseconds >(end - start).count() << "µs" << std::endl;
+        Matrix const m1{{0, -1}, {1, 0}};
+        Matrix const m2{{1, 2}, {3, 4}};
+        Matrix const expected{{-3, -4}, {1, 2}};
+        assert(m1 * m2 == expected);
     }
 }
-
---
+++ matrix.hpp
@@ -0,0 +1,200 @@
+#ifndef __CXX_DOJO_MATRIX_HPP__
+#define __CXX_DOJO_MATRIX_HPP__
+
+/*
+ * Website:
+ *      https://github.com/wo3kie/dojo
+ *
+ * Author:
+ *      Lukasz Czerwinski
+ */
+
+#include <iomanip>
+#include <vector>
+
+#include "./feq.hpp"
+#include "./output.hpp"
+
+class Matrix
+{
+public:
+    typedef std::vector<double> Row;
+
+public:
+    Matrix() = default;
+    Matrix(Matrix const &) = default;
+    Matrix(Matrix &&) = default;
+
+    Matrix & operator=(Matrix const &) = default;
+    Matrix & operator=(Matrix &&) = default;
+
+    Matrix(unsigned rows, unsigned columns, double init = 0.0)
+        : matrix_(rows, Row(columns, init))
+    {
+    }
+
+    Matrix(std::initializer_list<std::initializer_list<double>> const & data)
+        : matrix_(data.begin(), data.end())
+    {
+    }
+
+    Row & operator[](unsigned i){
+        return matrix_.at(i);
+    }
+
+    Row const & operator[](unsigned i)const{
+        return matrix_.at(i);
+    }
+
+    unsigned rows()const{
+        return matrix_.size();
+    }
+
+    unsigned columns()const{
+        if(rows() == 0){
+            return 0;
+        }
+
+        return matrix_[0].size();
+    }
+
+private:
+    std::vector<Row> matrix_;
+};
+
+inline
+std::ostream & operator<<(std::ostream & out, Matrix const & matrix){
+    auto getPrecision = [](double d){
+        return d < 0 ? 2 : 3;
+    };
+
+    out << "[";
+
+    if(matrix.rows() != 0){
+        out << "[";
+
+        if(matrix[0].size() != 0){
+            out << std::fixed << std::setfill(' ')
+                << std::setw(4) << std::setprecision(getPrecision(matrix[0][0]))
+                << matrix[0][0];
+
+            for(unsigned j = 1; j < matrix[0].size(); ++j){
+                out << ' ' << std::fixed << std::setfill(' ')
+                    << std::setw(4) << std::setprecision(getPrecision(matrix[0][j]))
+                    << matrix[0][j];
+            }
+        }
+
+        out << "]";
+
+        for(unsigned i = 1; i < matrix.rows(); ++i){
+            out << "\n [";
+
+            if(matrix[i].size() != 0){
+                out << std::fixed << std::setfill(' ')
+                    << std::setw(4) << std::setprecision(getPrecision(matrix[i][0]))
+                    << matrix[i][0];
+
+                for(unsigned j = 1; j < matrix[i].size(); ++j){
+                    out << ' ' << std::fixed << std::setfill(' ')
+                        << std::setw(4) << std::setprecision(getPrecision(matrix[i][j]))
+                        << matrix[i][j];
+                }
+            }
+
+            out << ']';
+        }
+    }
+
+    out << "]";
+
+    return out;
+}
+
+inline
+bool operator==(Matrix const & matrix1, Matrix const & matrix2){
+    unsigned const rows1 = matrix1.rows();
+    unsigned const rows2 = matrix2.rows();
+
+    if(rows1 != rows2){
+        return false;
+    }
+
+    for(unsigned row = 0; row < rows1; ++row){
+        unsigned const columns1 = matrix1[row].size();
+        unsigned const columns2 = matrix2[row].size();
+
+        if(columns1 != columns2){
+            return false;
+        }
+
+        for(unsigned column = 0; column < columns1; ++column){
+            if(!feq(matrix1[row][column], matrix2[row][column])){
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+inline
+bool operator!=(Matrix const & matrix1, Matrix const & matrix2){
+    return !(matrix1 == matrix2);
+}
+
+inline
+Matrix operator+(Matrix const & matrix1, double d){
+    Matrix result = Matrix(matrix1.rows(), matrix1.columns());
+
+    for(unsigned i = 0; i < matrix1.rows(); ++i){
+        for(unsigned j = 0; j < matrix1[0].size(); ++j){
+            result[i][j] = matrix1[i][j] + d;
+        }
+    }
+
+    return result;
+}
+
+inline
+Matrix operator+(Matrix const & matrix1, Matrix const & matrix2){
+    Matrix result = Matrix(matrix1.rows(), matrix1.columns());
+
+    for(unsigned i = 0; i < matrix1.rows(); ++i){
+        for(unsigned j = 0; j < matrix1.columns(); ++j){
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    return result;
+}
+
+inline
+Matrix operator*(Matrix const & matrix1, double d){
+    Matrix result = Matrix(matrix1.rows(), matrix1.columns());
+
+    for(unsigned i = 0; i < matrix1.rows(); ++i){
+        for(unsigned j = 0; j < matrix1[0].size(); ++j){
+            result[i][j] = matrix1[i][j] * d;
+        }
+    }
+
+    return result;
+}
+
+inline
+Matrix operator*(Matrix const & matrix1, Matrix const & matrix2){
+    Matrix result = Matrix(matrix1.rows(), matrix1.columns());
+
+    for(unsigned i = 0; i < matrix1.rows(); ++i){
+        for(unsigned j = 0; j < matrix2.columns(); ++j){
+            for(unsigned k = 0; k < matrix2.rows(); ++k){
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+#endif
