4f8c7c3cc9eea345403c208ca51b5af8e240fabb
wo3kie <wo3kie@hotmail.com>
2016-08-21 17:47:56 +0100

    generateAllSubsets

--- generateAllSubsets.cpp
+++ generateAllSubsets.cpp
@@ -17,39 +17,29 @@
 #include <set>
 #include <vector>
 
-std::set<std::set<int>> getAllSubsetsImpl(
-    std::set<std::set<int>> & sets1,
+#include "./output.hpp"
+
+std::set<std::set<int>> getAllSubsets(
     std::set<int> set
 ){
-    if(set.empty()){
-        return sets1;
+    std::set<std::set<int>> result;
+    result.insert(std::set<int>());
+
+    if(set.size() == 0){
+        return result;
     }
 
     int item = * set.begin();
     set.erase(set.begin());
+    std::set<std::set<int>> subsets = getAllSubsets(set);
 
-    std::set<std::set<int>> sets2;
-
-    for(std::set<int> s : sets1){
-        s.insert(item);
-        sets2.insert(s);
+    for(std::set<int> subset : subsets){
+        result.insert(subset);
+        subset.insert(item);
+        result.insert(subset);
     }
 
-    std::set<std::set<int>> subsets1 = getAllSubsetsImpl(sets1, set);
-    std::set<std::set<int>> subsets2 = getAllSubsetsImpl(sets2, set);
-
-    subsets1.insert(
-        subsets2.begin(),
-        subsets2.end()
-    );
-
-    return subsets1;
-}
-
-std::set<std::set<int>> getAllSubsets(std::set<int> const & set){
-    std::set<std::set<int>> sets;
-    sets.insert(std::set<int>());
-    return getAllSubsetsImpl(sets, set);
+    return result;
 }
 
 int main()
