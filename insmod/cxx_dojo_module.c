4b94c9399c269e6454853a9144203684226af10b
wo3kie <wo3kie@outlook.com>
2016-11-06 14:55:15 +0000

    insmod

--- insmod/cxx_dojo_module.c
+++ insmod/cxx_dojo_module.c
@@ -26,12 +26,23 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+
+static int param_int = 0;
+module_param(param_int, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+
+static char * param_string = "default";
+module_param(param_string, charp, 0);
+
 
 //int init_module(void)
 static int __init my_init(void)
 {
 	// printk is not a user space function like printf
 	printk("Hello cxx_dojo_module!\n");
+
+	printk("\tparam_int   : %d\n", param_int);
+	printk("\tparam_string: %s\n", param_string);
 
 	// return 0 to show loading was OK
 	return 0;
