e48f807f3a9d8dfbad8555fccaa8386fbc566c17
wo3kie <wo3kie@outlook.com>
2017-01-04 20:00:02 +0000

    modexp

--- modexp.cpp
+++ modexp.cpp
@@ -16,20 +16,7 @@
 #include <cmath>
 #include <iostream>
 
-constexpr unsigned pow2(unsigned a){
-	return a * a;
-}
-
-constexpr unsigned modexp(unsigned a, unsigned exp, unsigned mod){
-	return exp == 0
-		? 1
-		
-		//: exp % 2 == 0
-		//	? (    pow2(modexp(a, exp/2, mod))) % mod
-		//	: (a * pow2(modexp(a, exp/2, mod))) % mod;
-
-		: ( (exp % 2 == 0 ? 1 : a) * pow2(modexp(a, exp/2, mod)) % mod );
-}
+#include "./modexp.hpp"
 
 void modexp_test(){
 	for(unsigned a = 1; a < 10; ++a){
