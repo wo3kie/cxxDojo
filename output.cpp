6c8a33c9b6fcff070fba6d95b515006cd1a43831
wo3kie <wo3kie@outlook.com>
2018-08-20 21:04:36 +0100

    output.cpp

--- output.cpp
+++ output.cpp
@@ -14,6 +14,117 @@
 
 #include "./output.hpp"
 
+void output_tests()
+{
+    {
+        std::ostringstream oss; oss << std::vector< int >();
+        assert( std::string( "[]" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::vector< int >{ 1 };
+        assert( std::string( "[1]" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::vector< int >{ 1, 2 };
+        assert( std::string( "[1,2]" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::vector< int >{ 1, 2, 3 };
+        assert( std::string( "[1,2,3]" ) == oss.str() );
+    }
+
+    {
+        std::ostringstream oss; oss << std::array< int, 0 >();
+        assert( std::string( "[]" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::array< int, 1 >{ 1 };
+        assert( std::string( "[1]" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::array< int, 2 >{ 1, 2 };
+        assert( std::string( "[1,2]" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::array< int, 3 >{ 1, 2, 3 };
+        assert( std::string( "[1,2,3]" ) == oss.str() );
+    }
+
+    {
+        std::ostringstream oss; oss << std::list< int >();
+        assert( std::string( "()" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::list< int >{ 1 };
+        assert( std::string( "(1)" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::list< int >{ 1, 2 };
+        assert( std::string( "(1,2)" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::list< int >{ 1, 2, 3 };
+        assert( std::string( "(1,2,3)" ) == oss.str() );
+    }
+
+    {
+        std::ostringstream oss; oss << std::make_pair( 1, 2 );
+        assert( std::string( "{1,2}" ) == oss.str() );
+    }
+
+    {
+        std::ostringstream oss; oss << std::map< int, int >();
+        assert( std::string( "{}" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::map< int, int >{ { 1 , 2 } };
+        assert( std::string( "{{1,2}}" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::map< int, int >{ { 1 , 2 }, { 3, 4 } };
+        assert( std::string( "{{1,2},{3,4}}" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::map< int, int >{ { 1 , 2 }, { 3, 4 }, { 5, 6 } };
+        assert( std::string( "{{1,2},{3,4},{5,6}}" ) == oss.str() );
+    }
+
+    {
+        std::ostringstream oss; oss << std::set< int >();
+        assert( std::string( "{}" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::set< int >{ 1 };
+        assert( std::string( "{1}" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::set< int >{ 1, 2 };
+        assert( std::string( "{1,2}" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::set< int >{ 1, 2, 3 };
+        assert( std::string( "{1,2,3}" ) == oss.str() );
+    }
+
+    {
+        std::ostringstream oss; oss << std::unordered_map< int, int >();
+        assert( std::string( "{}" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::unordered_map< int, int >{ { 1 , 2 } };
+        assert( std::string( "{{1,2}}" ) == oss.str() );
+    }
+
+    {
+        std::ostringstream oss; oss << std::unordered_set< int >();
+        assert( std::string( "{}" ) == oss.str() );
+    }
+    {
+        std::ostringstream oss; oss << std::unordered_set< int >{ 1 };
+        assert( std::string( "{1}" ) == oss.str() );
+    }
+}
+
 int main(){
     output_tests();
 }
--- output.hpp
+++ output.hpp
@@ -21,6 +21,43 @@
 #include <unordered_set>
 #include <vector>
 
+template< typename T >
+std::ostream & operator<<( std::ostream & out, std::vector< T > const & v );
+
+template< typename T, std::size_t Size >
+std::ostream & operator<<( std::ostream & out, std::array< T, Size > const & a );
+
+template< typename Iterator >
+std::ostream & printList( std::ostream & out, Iterator begin, Iterator end );
+
+template< typename T >
+std::ostream & operator<<( std::ostream & out, std::list< T > const & v );
+
+template< typename T1, typename T2 >
+std::ostream & printPair( std::ostream & out, std::pair< T1, T2 > const & p );
+
+template< typename T1, typename T2 >
+std::ostream & operator<<( std::ostream & out, std::pair< T1, T2 > const & p );
+
+template< typename Iterator >
+std::ostream & printMap( std::ostream & out, Iterator begin, Iterator end );
+
+template< typename T1, typename T2 >
+std::ostream & operator<<( std::ostream & out, std::map< T1, T2 > const & m );
+
+template< typename Iterator >
+std::ostream & printSet( std::ostream & out, Iterator begin, Iterator end );
+
+template< typename T >
+std::ostream & operator<<( std::ostream & out, std::set< T > const & s );
+
+template< typename T1, typename T2 >
+std::ostream & operator<<( std::ostream & out, std::unordered_map< T1, T2 > const & u );
+
+template< typename T >
+std::ostream & operator<<( std::ostream & out, std::unordered_set< T > const & s );
+
+
 template< typename Iterator >
 std::ostream & printImpl(
     std::ostream & out,
@@ -108,115 +145,9 @@ std::ostream & operator<<( std::ostream & out, std::unordered_set< T > const & s
     return printSet( out, s.begin(), s.end() );
 }
 
-void output_tests()
-{
-    {
-        std::ostringstream oss; oss << std::vector< int >();
-        assert( std::string( "[]" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::vector< int >{ 1 };
-        assert( std::string( "[1]" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::vector< int >{ 1, 2 };
-        assert( std::string( "[1,2]" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::vector< int >{ 1, 2, 3 };
-        assert( std::string( "[1,2,3]" ) == oss.str() );
-    }
-
-    {
-        std::ostringstream oss; oss << std::array< int, 0 >();
-        assert( std::string( "[]" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::array< int, 1 >{ 1 };
-        assert( std::string( "[1]" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::array< int, 2 >{ 1, 2 };
-        assert( std::string( "[1,2]" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::array< int, 3 >{ 1, 2, 3 };
-        assert( std::string( "[1,2,3]" ) == oss.str() );
-    }
-
-    {
-        std::ostringstream oss; oss << std::list< int >();
-        assert( std::string( "()" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::list< int >{ 1 };
-        assert( std::string( "(1)" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::list< int >{ 1, 2 };
-        assert( std::string( "(1,2)" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::list< int >{ 1, 2, 3 };
-        assert( std::string( "(1,2,3)" ) == oss.str() );
-    }
-
-    {
-        std::ostringstream oss; oss << std::make_pair( 1, 2 );
-        assert( std::string( "{1,2}" ) == oss.str() );
-    }
-
-    {
-        std::ostringstream oss; oss << std::map< int, int >();
-        assert( std::string( "{}" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::map< int, int >{ { 1 , 2 } };
-        assert( std::string( "{{1,2}}" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::map< int, int >{ { 1 , 2 }, { 3, 4 } };
-        assert( std::string( "{{1,2},{3,4}}" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::map< int, int >{ { 1 , 2 }, { 3, 4 }, { 5, 6 } };
-        assert( std::string( "{{1,2},{3,4},{5,6}}" ) == oss.str() );
-    }
-
-    {
-        std::ostringstream oss; oss << std::set< int >();
-        assert( std::string( "{}" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::set< int >{ 1 };
-        assert( std::string( "{1}" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::set< int >{ 1, 2 };
-        assert( std::string( "{1,2}" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::set< int >{ 1, 2, 3 };
-        assert( std::string( "{1,2,3}" ) == oss.str() );
-    }
-
-    {
-        std::ostringstream oss; oss << std::unordered_map< int, int >();
-        assert( std::string( "{}" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::unordered_map< int, int >{ { 1 , 2 } };
-        assert( std::string( "{{1,2}}" ) == oss.str() );
-    }
-
-    {
-        std::ostringstream oss; oss << std::unordered_set< int >();
-        assert( std::string( "{}" ) == oss.str() );
-    }
-    {
-        std::ostringstream oss; oss << std::unordered_set< int >{ 1 };
-        assert( std::string( "{1}" ) == oss.str() );
-    }
+template<typename T>
+void print(T const & val, char end='\n'){
+    std::cout << val << end;
 }
 
 #endif
