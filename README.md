a0fad5d42ef39088646da908517837ca9f589c27
wo3kie <wo3kie@outlook.com>
2025-09-12 22:07:12 +0200

    Update dijkstra.cpp

--- README.md
+++ README.md
@@ -129,6 +129,9 @@ Implement a CRTP pattern example. From [Wikipedia](https://en.wikipedia.org/wiki
 **ctad.cpp**  
 Implement some examples showing class template argument deduction (CTAD) usage.  
   
+**dijkstra.cpp**  
+Implement Dijkstra algorithm. From [Wikipedia](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm) the Dijkstra algorithm is a procedure for finding the shortest paths between nodes in a weighted graph.
+  
 **echo.server/echo.client**  
 Implement simple echo server and text line based client to work with server.
 ```{r, engine='bash'}
@@ -504,9 +507,6 @@ lis( {1, 9, 2, 8, 3, 7, 4} ) == { 1, 2, 3, 4 } )
 Implement a program representing the type erasure idiom.
 
 From [Wikibooks](https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Type_Erasure) type erasure is a mechanism to provide a type-neutral container that interfaces a variety of concrete types. An example can be an implementation of [boost::any](http://www.boost.org/doc/libs/1_61_0/doc/html/any.html) and also a way how does arbitrary deleter type can be used in [std::shared_ptr](http://en.cppreference.com/w/cpp/memory/shared_ptr).
-
-**dijkstra.cpp**  
-Implement Dijkstra algorithm.
 
 **floydWarshall.cpp**  
 Implement Floyd-Warshall algorithm.
--- dijkstra.cpp
+++ dijkstra.cpp
@@ -12,85 +12,22 @@
  *      $ ./dijkstra
  */
 
-#include <algorithm>
-#include <cassert>
-#include <limits>
-#include <vector>
-
+#include "./dijkstra.hpp"
 #include "./output.hpp"
 
 /*
- * S. Dasgupta, C. Papadimitrioui U. Vazirani, Algorithms
+ * dijkstra_test
  */
-
-typedef std::vector<std::vector<int>> Matrix;
-
-std::pair<std::vector<int> /*dist*/, std::vector<int> /*prev*/> dijkstra(Matrix const& graph, int start = 0) {
-  int const size = graph.size();
-
-  std::vector<int> dist(size, std::numeric_limits<int>::max());
-  std::vector<int> prev(size, -1);
-  dist[start] = 0;
-
-  auto cmp = [&](int i1, int i2) {
-    return dist[i1] > dist[i2];
-  };
-
-  std::vector<int> H(size, 0);
-
-  for(int i = 0; i < size; ++i) {
-    H[i] = i;
-  }
-
-  std::make_heap(H.begin(), H.end(), cmp);
-
-  while(H.empty() == false) {
-    std::pop_heap(H.begin(), H.end(), cmp);
-    int const u = H.back();
-    H.pop_back();
-
-    for(int v = 0; v < size; ++v) {
-      if(u == v) {
-        continue;
-      }
-
-      if(graph[u][v] == -1) {
-        continue;
-      }
-
-      if(dist[v] > dist[u] + graph[u][v]) {
-        dist[v] = dist[u] + graph[u][v];
-        prev[v] = u;
-
-        std::make_heap(H.begin(), H.end(), cmp);
-      }
-    }
-  }
-
-  return std::make_pair(dist, prev);
-}
-
-std::vector<int> decodeShortestPath(std::vector<int> const& prev, int const start, int end) {
-  std::vector<int> path(1, end);
-
-  while(prev[end] != -1) {
-    path.push_back(prev[end]);
-    end = prev[end];
-  }
-
-  std::reverse(path.begin(), path.end());
-
-  return path;
-}
-
-int main() {
+void dijkstra_test() {
+  // clang-format off
   Matrix matrix{
-      {0, 4, 2, -1, -1},
-      {-1, 0, 3, 2, 3},
-      {-1, 1, 0, 4, 5},
-      {-1, -1, -1, 0, -1},
-      {-1, -1, -1, 1, 0},
+      { 0,  4,  2, -1, -1},
+      {-1,  0,  3,  2,  3},
+      {-1,  1,  0,  4,  5},
+      {-1, -1, -1,  0, -1},
+      {-1, -1, -1,  1,  0},
   };
+  // clang-format on
 
   std::vector<int> dist;
   std::vector<int> prev;
@@ -107,4 +44,11 @@ int main() {
   assert((decodeShortestPath(prev, 0, 2) == VI{0, 2}));
   assert((decodeShortestPath(prev, 0, 3) == VI{0, 2, 1, 3}));
   assert((decodeShortestPath(prev, 0, 4) == VI{0, 2, 1, 4}));
+}
+
+/*
+ * main
+ */
+int main() {
+  dijkstra_test();
 }
