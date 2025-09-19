b2966ce51abab8b5d631960d206559238608bd63
wo3kie <wo3kie@outlook.com>
2025-09-19 22:12:24 +0200

    Update gsl/correlation.cpp

--- gsl/correlation.cpp
+++ gsl/correlation.cpp
@@ -14,6 +14,7 @@
 
 #include <gsl/gsl_statistics.h>
 
+#include "../correlation.hpp"
 #include "../feq.hpp"
 
 /*
@@ -24,6 +25,13 @@ int main() {
   const std::vector<double> array1{1.2, 2.0, 2.5, 4.0, 3.0, 6.0, 5.5, 6.3, 7.1, 5.4};
   const std::vector<double> array2{3.4, 3.3, 3.0, 5.5, 1.2, 2.4, 3.2, 3.1, 2.9, 3.2};
 
-  const double correlationValue = gsl_stats_correlation(&array1.front(), 1, &array2.front(), 1, array2.size());
-  assert(feq(correlationValue, -0.050601));
+  {
+    const double correlationValue = correlation(array1, array2);
+    assert(feq(correlationValue, -0.050601));
+  }
+
+  {
+    const double correlationValue = gsl_stats_correlation(&array1.front(), 1, &array2.front(), 1, array2.size());
+    assert(feq(correlationValue, -0.050601));
+  }
 }
