a6436371260f1c52b22b43d48cd40a869553b732
wo3kie <wo3kie@outlook.com>
2026-03-20 19:06:18 +0100

    Update reverse_words.cpp

--- reverse_words.cpp
+++ reverse_words.cpp
@@ -4,30 +4,28 @@
  *
  * Author:
  *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
- *
- * Usage:
- *      $ ./build/bin/reverseWords
  */
 
 #include <algorithm>
 #include <cmath>
 #include <functional>
 #include <iostream>
+#include <ranges>
 #include <string>
 #include <vector>
 
 #include "./assert.hpp"
 
 /*
- * reverseWords
+ * reverse_words
  */
 
-std::string reverseWords(std::string string) {
+std::string reverse_words(std::string string) {
   std::reverse(string.begin(), string.end());
 
-  int wordBegin = 0;
-  int wordEnd = 0;
-  const int end = string.size();
+  size_t wordBegin = 0;
+  size_t wordEnd = 0;
+  const size_t end = string.size();
 
   while(wordEnd < end) {
     while(wordEnd < end) {
@@ -47,10 +45,27 @@ std::string reverseWords(std::string string) {
   return string;
 }
 
+namespace ranges {
+
+std::string reverse_words(std::string s) {
+    std::reverse(s.begin(), s.end());
+
+    for(auto&& word : s | std::views::split(' ')) {
+        auto begin = word.begin();
+        auto end = word.end();
+        std::reverse(begin, end);
+    }
+
+    return s;
+}
+
+}
+
 /*
  * main
  */
 
 int main() {
-  Assert(reverseWords("This is a test") == "test a is This");
+  Assert(reverse_words("This is a test") == "test a is This");
+  Assert(ranges::reverse_words("This is a test") == "test a is This");
 }
