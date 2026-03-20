b4475a8e822126bea9b721b70f923d8d95f746e3
wo3kie <wo3kie@outlook.com>
2026-03-20 19:05:31 +0100

    Remove old dojos

--- gsl/mean.cpp
+++ gsl/mean.cpp
@@ -14,14 +14,10 @@
 
 #include "../assert.hpp"
 #include "../feq.hpp"
-#include "../mean.hpp"
 
 int main() {
   std::vector<double> const array1{1.0, 2.0, 3.0};
   std::vector<double> const array2{-1.0, 0, 1.0};
-
-  Assert(2.0 == approx(mean<double>(array1)));
-  Assert(0.0 == approx(mean<double>(array2)));
 
   Assert(2.0 == approx(gsl_stats_mean(&array1.front(), 1, array1.size())));
   Assert(0.0 == approx(gsl_stats_mean(&array2.front(), 1, array2.size())));
--- gsl/variance.cpp
+++ gsl/variance.cpp
@@ -4,9 +4,6 @@
  *
  * Author:
  *      Lukasz Czerwinski (https://www.lukaszczerwinski.pl/)
- *
- * Usage:
- *      $ ./build/bin/gsl_variance
  */
 
 
@@ -14,27 +11,13 @@
 
 #include "../assert.hpp"
 #include "../feq.hpp"
-#include "../variance.hpp"
 
 /*
  * main
  */
 
 int main() {
-  {
-    std::vector<double> array{7, 6, 8, 4, 2, 7, 6, 7, 6, 5};
+  std::vector<double> array{7, 6, 8, 4, 2, 7, 6, 7, 6, 5};
 
-    Assert(3.0666 == approx(variance<double>(array)));
-    Assert(3.0666 == approx(variance2<double>(array)));
-    Assert(3.0666 == approx(variance3<double>(array)));
-    Assert(3.0666 == approx(gsl_stats_variance(&array.front(), 1, array.size())));
-  }
-  {
-    std::vector<double> array{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-    Assert(9.1666 == approx(variance<double>(array)));
-    Assert(9.1666 == approx(variance2<double>(array)));
-    Assert(9.1666 == approx(variance3<double>(array)));
-    Assert(9.1666 == approx(gsl_stats_variance(&array.front(), 1, array.size())));
-  }
+  Assert(3.0666 == approx(gsl_stats_variance(&array.front(), 1, array.size())));
 }
